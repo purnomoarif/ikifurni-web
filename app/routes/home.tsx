a24e9e66460dbb4c0315a50208e4d007e7a69e7a
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-10-14 20:28:38 +0700

    feat: implement product loader and define product types in Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { log } from "console";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -7,7 +8,27 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+type Product = {
+  id: string;
+  name: string;
+  description?: string | null;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type Products = Product[];
+
+export async function loader({ params }: Route.ClientLoaderArgs) {
+  const response = await fetch(`http://localhost:3000/products`);
+  const products = await response.json();
+  return products;
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const products = loaderData;
+
+  console.log({ products });
   return (
     <div>
       <h1>Ikifurni</h1>
