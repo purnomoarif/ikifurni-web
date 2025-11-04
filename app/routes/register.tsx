e140022fb2fbc716c8a46f0291132546c18e14ab
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-11-04 10:34:16 +0700

    feat: improve register form style

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,4 +1,8 @@
+import { Form } from "react-router";
 import type { Route } from "./+types/register";
+import { Label } from "~/components/ui/label";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Register" }];
@@ -6,8 +10,56 @@ export function meta({}: Route.MetaArgs) {
 
 export default function RegisterRoute({}: Route.ComponentProps) {
   return (
-    <div>
-      <h1>Create new account</h1>
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4">
+      <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-md">
+        <h1 className="mb-6 text-center text-2xl font-semibold text-gray-800">
+          Create New Account
+        </h1>
+
+        <Form method="post" className="space-y-5">
+          <div className="space-y-2">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              id="username"
+              name="username"
+              type="text"
+              placeholder="Iki"
+              required
+            />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="ikifurni@ikifurni.com"
+              required
+            />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="fullName">Full Name</Label>
+            <Input
+              id="fullName"
+              name="fullName"
+              type="text"
+              placeholder="Ikifurni"
+              required
+            />
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" name="password" type="password" />
+          </div>
+
+          <Button type="submit" className="w-full">
+            Create Account
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
